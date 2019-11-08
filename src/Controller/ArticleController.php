a2aa229d71afdf5135e213c28157a657f5511424
fernandohous <fernando.b10@aluno.ifsc.edu.br>
2019-11-08 08:00:30 -0200

    First commit

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('First Page');
+    }
+
+}
