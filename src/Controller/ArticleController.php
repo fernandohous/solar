6296e8e0a43796197dfda7005832d81ea8102c95
fernandohous <fernando.b10@aluno.ifsc.edu.br>
2019-11-08 10:13:09 -0200

    First interaction with twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,10 +4,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -15,6 +16,16 @@ class ArticleController
     public function homepage()
     {
         return new Response('First Page');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return $this->render('/article/show.html.twig',[
+            'title' => ucwords(str_replace('-',' ', $slug))
+        ]);
     }
 
 }
