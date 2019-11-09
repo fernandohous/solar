76d5c060d274c71e2d441485fe6e7df4dd589fa7
fernandohous <fernando.b10@aluno.ifsc.edu.br>
2019-11-09 02:53:29 -0200

    Add framework Ulkit / css

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
